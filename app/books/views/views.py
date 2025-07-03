4f32eae105a869f8c1be94812e1da6ab3da9e863
user <morevstas02@mail.ru>
2025-07-03 17:59:52 +0300

    а

--- app/books/views/views.py
+++ app/books/views/views.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.books.Service.Dependency import get_session
 from app.books.Service.service import create_autor, get_autor_by_id, get_autor_by_all, get_autor_by_del, \
-    get_autor_by_update
+    get_autor_by_update, get_autor_with_books
 from app.books.schemas.Schemas import AutorCreate, AutorUpdate, AutorBase
 
 router_autors_and_books = APIRouter(tags=['autors'],prefix='/autors')
@@ -28,15 +28,6 @@ async def autor_all(session: AsyncSession = Depends(get_session)):
     authors = await get_autor_by_all(session)
     return authors
 
-
-@router_autors_and_books.delete('/{autors_id}/')
-async def autor_delete(autor_id:int,session:AsyncSession = Depends(get_session)):
-    delet_autor = await get_autor_by_del(session=session,autor_id=autor_id)
-    if delet_autor is None:
-        raise HTTPException(status_code=404, detail="Author not found")
-    return delet_autor
-
-
 @router_autors_and_books.put('/{autor_id}/')
 async def update_autor(
         autor_data:AutorUpdate,
@@ -48,9 +39,23 @@ async def update_autor(
         raise HTTPException(status_code=404, detail="Author not found")
     return update_autors
 
+@router_autors_and_books.delete('/{autors_id}/')
+async def autor_delete(autor_id:int,session:AsyncSession = Depends(get_session)):
+    delet_autor = await get_autor_by_del(session=session,autor_id=autor_id)
+    if delet_autor is None:
+        raise HTTPException(status_code=404, detail="Author not found")
+    return delet_autor
 
 
 
+
+
+@router_autors_and_books.get("/{author_id}/")
+async def read_author(author_id: int, session: AsyncSession = Depends(get_session)):
+    author = await get_autor_with_books(session=session, autor_id=author_id)
+    if author is None:
+        raise HTTPException(status_code=404, detail="Author not found")
+    return author
 
 
 

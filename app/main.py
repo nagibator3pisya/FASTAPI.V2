d33297cd0c4ed8183e4bd6d10fbe0dcb861b2384
user <morevstas02@mail.ru>
2025-07-04 17:19:37 +0300

    body параметры, pydantic

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 
 app = FastAPI()
 """
@@ -13,10 +13,16 @@ PUT (изменение данных) - update
 DELETE (удаление данных) - delete
 """
 
-
+# Query-параметры http://127.0.0.1:8000/items/?skip=0&limit=10
 @app.get("/")
-async def root(name:str):
-    return {"message": f"Hello {name}"}
+async def root(name:str,age:int):
+    return {"message": f"Hello {name},age {age}"}
+
+
+# отличие между auery параметрами и body параметрами
+@app.post("/")
+async def root(name:str,age:int):
+    return {"message": f"Hello {name},age {age}"}
 
 
 if __name__ == '__main__':

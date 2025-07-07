597645023e76415880447278b5bcc1c1155073b2
user <morevstas02@mail.ru>
2025-07-07 14:14:37 +0300

    Модели Pydantic

--- app/model.py
+++ app/model.py
@@ -1,11 +1,11 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 
 
 
 class Phone(BaseModel):
-    phone: int
+    phone:  Optional[int] = None
 
 
 
@@ -13,17 +13,17 @@ class Phone(BaseModel):
 class Address(BaseModel):
     country: str
     city : str
-    phones: Phone # 1 адрес может иметь 1 телефон
+    # phones: Phone # 1 адрес может иметь 1 телефон
 
 class User(BaseModel):
-    name:str
-    age: int
+    name:str = Field(min_length=1)
+    age: int = Field(gt=1, lt=100)
     description: Optional[str] = None # не обязательный параметр
     address: Optional[list[Address]] =  None # связь с адресом
 
 
 address = Address(country='Россия',city='Москва')
-user = User(name='Саша',age=22,address=[address])
+user = User(name='Саша',age=3,address=[address])
 
 print(user.model_dump())
 

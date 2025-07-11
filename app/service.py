a245d78b1ba5133c90fb48937e1aa0557cf02ca3
user <morevstas02@mail.ru>
2025-07-11 15:35:07 +0300

    удаление и обновление crud

--- app/service.py
+++ app/service.py
@@ -2,7 +2,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app import schemas
-from app.schemas import TaskCreate
+from app.schemas import TaskCreate, TaskUpdate
 from model import Task
 
 async def get_task_first(session:AsyncSession,task_id:int):
@@ -14,10 +14,6 @@ async def get_task_all(session:AsyncSession):
     return result.scalars().all()
 
 
-
-
-
-
 async  def create_task(session:AsyncSession, task:TaskCreate):
     result =Task(**task.model_dump())
     session.add(result)
@@ -25,3 +21,27 @@ async  def create_task(session:AsyncSession, task:TaskCreate):
     return result
 
 
+async def update_task(task_id: int,task_data: TaskUpdate,session:AsyncSession):
+    result = await session.execute(select(Task).where(Task.id == task_id))
+    task =result.scalar_one_or_none()
+
+    if not task:
+        return None
+
+    for k,v in task_data.model_dump().items():
+        setattr(task,k,v)
+    await session.commit()
+    await session.refresh(task)
+    return task
+
+
+async def delete_task(task_id: int, session: AsyncSession):
+    result = await session.execute(select(Task).where(Task.id == task_id))
+    task = result.scalar_one_or_none()
+
+    if not task:
+        return None
+
+    await session.delete(task)
+    await session.commit()
+    return {"detail": "Задача удалена"}

1f7a67ce03f0d430a891e74abb9eb267ceab7f8e
user <morevstas02@mail.ru>
2025-07-17 18:03:09 +0300

    проверка

--- demo/views.py
+++ demo/views.py
@@ -1,14 +1,16 @@
+import secrets
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException,status
 from fastapi.security import HTTPBasic,HTTPBasicCredentials
+
 routers_auts = APIRouter(prefix='/demo-auth',tags=['demo-auth'])
 
 
 httpbasic = HTTPBasic()
 
 # Аутентификация — это процесс проверки подлинности пользователя
-@routers_auts.get('/basuc-auth/')
+@routers_auts.get('/basic-auth/')
 def demo_auth(
         credentials: Annotated[HTTPBasicCredentials, Depends(httpbasic)]
 ):
@@ -19,12 +21,56 @@ def demo_auth(
    }
 
 
+user = {
+    'admin': 'admin',
+    'join': ' password'
+}
+
+@routers_auts.get('/basic-auth/')
+def get_auth_user(
+        credentials: Annotated[HTTPBasicCredentials, Depends(httpbasic)]
+):
+  unated = HTTPException(
+      status_code =status.HTTP_401_UNAUTHORIZED,
+      detail='Invalid username or password',
+      headers = {'WWW-Authenticate':'Basic'}
+  )
+  correst_password = user.get(credentials)
+  # if credentials.username not in user:
+  #     raise unated
+  # if not secrets.compare_digest(
+  #         credentials.password.encode('utf-8')
+  # ):
+  #     raise unated
+
+
+
+
+
 # проверка юзера
-@routers_auts.get('/auth/')
+@routers_auts.get('/some-http-header-auth/')
 def demo_auth_username(
-        aut_username: str = Depends()
+        username: str = Depends()
 ):
    return {
-       'message': F'hi {aut_username}',
-       'user_name':aut_username,
+       'message': F'hi {username}',
+       'user_name':username,
    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

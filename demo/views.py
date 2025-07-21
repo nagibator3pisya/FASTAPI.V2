80796dad337906274ad17f31e6bd9b089330df75
user <morevstas02@mail.ru>
2025-07-21 11:37:42 +0300

    базовая аутентификация

--- demo/views.py
+++ demo/views.py
@@ -26,7 +26,7 @@ user = {
     'join': ' password'
 }
 
-@routers_auts.get('/basic-auth/')
+
 def get_auth_user(
         credentials: Annotated[HTTPBasicCredentials, Depends(httpbasic)]
 ):
@@ -35,22 +35,22 @@ def get_auth_user(
       detail='Invalid username or password',
       headers = {'WWW-Authenticate':'Basic'}
   )
-  correst_password = user.get(credentials)
-  # if credentials.username not in user:
-  #     raise unated
-  # if not secrets.compare_digest(
-  #         credentials.password.encode('utf-8')
-  # ):
-  #     raise unated
+  correst_password = user.get(credentials.username)
+  if user is None:
+      raise unated
+
+  if not secrets.compare_digest(
+          credentials.password.encode('utf-8'),
+           correst_password.encode('utf-8')
+  ):
+      raise unated
+  return credentials.username
 
 
-
-
-
-# проверка юзера
-@routers_auts.get('/some-http-header-auth/')
+# проверка юзера если не верное то не попадаем
+@routers_auts.get('/basic-aut/')
 def demo_auth_username(
-        username: str = Depends()
+        username: str = Depends(get_auth_user)
 ):
    return {
        'message': F'hi {username}',

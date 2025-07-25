61252fa4ba4208321ac7ef661f950f114b4a731f
user <morevstas02@mail.ru>
2025-07-25 11:20:19 +0300

    база

--- demo/views.py
+++ demo/views.py
@@ -1,43 +1,22 @@
 import secrets
-from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, status, Header
-from fastapi.security import HTTPBasic,HTTPBasicCredentials
+from typing import Annotated, Any
+
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+from app.schemas import UserLoginSchema
+from authx import AuthXConfig, AuthX
+from fastapi import APIRouter, HTTPException, security, Depends
 
 routers_auts = APIRouter(prefix='/demo-auth',tags=['demo-auth'])
 
+# базовая
+security = HTTPBasic()
 
 
-
-static_auth_token_to_username= {
-    'f438578fd7b885f11acdafdb411cdb3c7644f38442bf26aca71d26a92fe37aec': 'admin',
-    'e448cbf29162c6941d6a251bb13c2d4d330a7e981a4e42c41a4e37b2385f58c5': ' jon'
-}
-
-
-def get_user_by_static_auth_token(
-        statik_token: str = Header(alias='x-auth-token'),
-):
-    if username := static_auth_token_to_username.get(statik_token):
-        return username
-    raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail='token invalid'
-    )
-
-
-
-
-# проверка юзера если не верное то не попадаем
-@routers_auts.get('/some-http-header-auth/')
-def demo_auth_some_http_header(
-        username: str = Depends(get_user_by_static_auth_token)
-):
-   return {
-       'message': F'hi {username}',
-       'user_name':username,
-   }
-
+@routers_auts.get("/users/me")
+def read_current_user(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    return {"username": credentials.username, "password": credentials.password}
 
 
 
